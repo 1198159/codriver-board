4b64970a52f6268563871be2d2c2cfe589b341b4
timothyeng36@gmail.com <Timothy Eng>
2019-03-06 18:39:56 -0800

    Set joystick x and y axes based on analog values from potentiometers.

--- USBJoystick/USBJoystick.ino
+++ USBJoystick/USBJoystick.ino
@@ -8,6 +8,21 @@ const int digitalInputID = 22;
 // How many buttons (and digital inputs) we will use.
 const unsigned int MAX_BUTTONS = 8;
 
+// The analog joystick pins.
+int joyPin0 = 0;
+int joyPin1 = 1;
+int median = 606;
+
+// Range values
+int minAxisVal = -32768;
+int maxAxisVal = 32767;
+
+int minAnalogVal = 0;
+int maxAnalogVal = 1023;
+
+int midAnalogXVal = 700;
+int midAnalogYVal = 530;
+
 void setup() {
   // put your setup code here, to run once:
   Joystick.clearState();
@@ -16,13 +31,38 @@ void setup() {
   for(int buttonID = 0; buttonID < MAX_BUTTONS; buttonID++) {
     pinMode(digitalInputID + buttonID, INPUT_PULLUP);
   }
+  Serial.begin(9600);
 }
 
+// Converts data from one range [a1, b1] to another [a2, b2].
+int convertRange(int data, int a1, int b1, int a2, int b2) {
+  return (data - a1) * (b2 - a2) / (b1 - a1) + a2;
+}
+
+// Converts analog data to values for the usb joystick.
+int analogToStick(int rawData, int midVal) {
+  int convertedData;
+  if(rawData >= midVal) {
+    convertedData = convertRange(rawData, midVal, maxAnalogVal, 0, maxAxisVal);
+  } else {
+    convertedData = convertRange(rawData, minAnalogVal, midVal, minAxisVal, 0);
+  }
+  
+  Serial.println(rawData);
+  Serial.println(convertedData);
+
+  return convertedData;
+}
 void loop() {
   // put your main code here, to run repeatedly:
   Joystick.clearState();
+  
   // Set the joystick state
-  Joystick.state.x.axis = -32768;
+  int rawXAxisData = analogRead(joyPin0);
+  Joystick.state.x.axis = analogToStick(rawXAxisData, midAnalogXVal);
+
+  int rawYAxisData = analogRead(joyPin1);
+  Joystick.state.y.axis = analogToStick(rawYAxisData, midAnalogYVal);
 
   // Set the joystick buttons according to the respective IDs
   for(int buttonID = 0; buttonID < MAX_BUTTONS; buttonID++) {
